c67357bf891d45550b5a91652e237ff57627e55b
huangjx <trustfor@sina.cn>
2018-05-06 23:51:14 +0800

    添加jwt 生成登录时候的token 用于用户操作鉴权

--- node004_1/one_punch/routes/index.js
+++ node004_1/one_punch/routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../modules/mongo/user')
+// TOKEN生成
+const JWT = require('jsonwebtoken');
+// 加密的密钥
+const JWT_SECRET = require('../cipher').JWT_SECRET;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -20,10 +24,15 @@ router.post('/signUp',function(req,res,next){
 router.post('/login',function(req,res,next){
   (async ()=>{
     const user = await User.login(req.body.phoneNumber,req.body.password)
+    console.log(1)
+    // 此处需要过期时间 和密钥
+    const token = JWT.sign({_id:user._id,iat:Date.now(),expire:Date.now()+20000},JWT_SECRET);
+    console.log(2)
     return {
       code:0,
       data:{
-        user:user
+        user:user,
+        token:token
       }
     }
   })()
